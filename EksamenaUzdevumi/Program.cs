8fbbc37bd2724ac5e64e08aa27b29bf5b631d582
rebeka.pilsikova <rebeka.pilscikova@visma.com>
2025-04-07 10:12:18 +0300

    07.04

--- EksamenaUzdevumi/Program.cs
+++ EksamenaUzdevumi/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class CssdCar
+{
+    public string Zimols { get; set; }
+    public string Modelis { get; set; }
+    public string RegistracijasDatums { get; set; }
+    public int PilnaMasa { get; set; }
+    public string DegvielasVeids { get; set; }
+
+    public CssdCar(string mark, string model, string registrationtime, int height, string fuel)
+    {
+        this.Zimols = mark;
+        this.Modelis = model;
+        this.RegistracijasDatums = registrationtime;
+        this.PilnaMasa = height;
+        this.DegvielasVeids = fuel;
+    }
+
+    public void PritVehicles()
+    {
+        Console.WriteLine($"Mark{Zimols} Model{Modelis} Registrationtime{RegistracijasDatums} height{PilnaMasa} fuel{DegvielasVeids}");
+    }
+}
